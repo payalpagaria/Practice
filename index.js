d2b4fb3841057e6ffbc302835978c8440082c532
payalpagaria <pagariapayal67@gmail.com>
2023-07-18 00:20:40 +0530

    changes

--- index.js
+++ index.js
@@ -1,121 +1,27 @@
 let taskInputList = [];
-let taskInput = document.getElementById("taskInput");
-let submitBtn = document.getElementById("btn");
+const storedTaskInputList = localStorage.getItem('taskInputList');
+if (storedTaskInputList) {
+    taskInputList = JSON.parse(storedTaskInputList);
+}
+
+const submitBtn = document.getElementById("btn");
 let ulList = document.getElementById("list");
-let taskDetails = document.createElement('p');
-
-submitBtn.addEventListener('click', handleClick);
-
-function handleClick() {
-    taskInputList.push(taskInput.value);
-    taskInput.value = ''; // Clear the input field
-
-    ulList.innerHTML = '';
-
-    showList();
-}
-function handleSwiperight(taskItem, index) {
-    let touchStartX = 0;
-    let touchEndX = 0;
-  
-    taskItem.addEventListener('touchstart', function (event) {
-      touchStartX = event.touches[0].clientX;
+ 
+function onSubmit(){
+    const taskTitleInput=document.getElementById("titleInput").value;
+    const taskInput = document.getElementById("taskInput").value;
+    console.log("title:",taskTitleInput);
+    taskInputList.push({
+        taskTitleInput,
+        taskInput
     });
-  
-    taskItem.addEventListener('touchend', function (event) {
-      touchEndX = event.changedTouches[0].clientX;
-      if (touchEndX > touchStartX) {
-        taskDetails.textContent=taskInputList[index];
-      }
-    });
-   
-  }
-  
-function handleDelete(index) {
-    taskInputList.splice(index, 1);
-    ulList.innerHTML = '';
-    showList();
+    taskInput.value = '';
+    taskTitleInput.value=''; // Clear the input field
+    saveTaskListToStorage();
+    ulList.innerHTML='';
+
+
+function saveTaskListToStorage() {
+    localStorage.setItem('taskInputList', JSON.stringify(taskInputList));
 }
-
-function handleUpdate(index) {
-    let updateMsg = prompt('Enter the updated message:', taskInputList[index]);
-    if (updateMsg) {
-        taskInputList[index] = updateMsg;
-        ulList.innerHTML = '';
-        showList();
-    }
-}
-
-function truncateText(text, maxLength) {
-    if (text.length > maxLength) {
-        return text.substring(0, maxLength) + '...';
-    }
-    return text;
-}
-function handleSwipeLeft(taskItem, index) {
-    let touchStartX = 0;
-    let touchEndX = 0;
-
-    taskItem.addEventListener('touchstart', function(event) {
-        touchStartX = event.touches[0].clientX;
-    });
-
-    taskItem.addEventListener('touchend', function(event) {
-        touchEndX = event.changedTouches[0].clientX;
-        if (touchEndX < touchStartX) {
-            handleDelete(index);
-        }
-    });
-}
-
-function showList() {
-    const maxLines = 1; // Maximum number of lines to display
-    const maxCharsPerLine = 15; // Maximum number of characters per line
-    for (let i = 0; i < taskInputList.length; i++) {
-        let newLi = document.createElement('li');
-        let newDel = document.createElement('button');
-        let newUpdate = document.createElement('button');
-        let taskHeading = document.createElement('h3');
-        let taskName = document.createElement('label');
-        taskHeading.textContent = `Task ${i + 1} :`;
-        taskName.textContent = taskInputList[i];
-        
-        taskDetails.textContent = truncateText(taskInputList[i], maxCharsPerLine * maxLines);
-
-        newDel.textContent = 'Delete';
-        newUpdate.textContent = 'Update';
-
-        ulList.appendChild(newLi);
-        newLi.appendChild(taskHeading);
-       // newLi.appendChild(taskName);
-        newLi.appendChild(taskDetails);
-        newLi.appendChild(newDel);
-        newLi.appendChild(newUpdate);
-
-        newDel.addEventListener('click', function() {
-            handleDelete(i);
-        });
-
-        newUpdate.addEventListener('click', function() {
-            handleUpdate(i);
-        });
-        
-        // Swipe left gesture to delete task
-        // Swipe left gesture to delete task
-           handleSwipeLeft(newLi, i);
-            handleSwiperight(newLi,i);
-        // Click to expand/collapse task details
-        let isExpanded = false;
-        taskHeading.addEventListener('click', function() {
-            if (isExpanded) {
-                taskDetails.textContent = truncateText(taskInputList[i], maxCharsPerLine * maxLines);
-                taskDetails.style.height = 'auto';
-                isExpanded = false;
-            } else {
-                taskDetails.textContent = taskInputList[i];
-                taskDetails.style.height = '5em';
-                isExpanded = true;
-            }
-        });
-    }
 }
---
+++ todo.js
@@ -0,0 +1,137 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const ulList = document.getElementById("todoList");
+    taskInputList = getTaskListFromStorage();
+
+    showList();
+});
+
+let searchBtn=document.getElementById("searchBtn");
+
+searchBtn.addEventListener('click',()=>{
+    let searchInp=document.getElementById("searchInput");
+    handleSearch(searchInp.value);
+})
+function handleSearch(searchText){
+    let status=false;
+    searchText = searchText.toLowerCase();
+
+    for(let i=0;i<taskInputList.length;i++){
+        const taskTitle = taskInputList[i].taskTitleInput.toLowerCase();
+        if(taskTitle==searchText){
+            showTaskDetails(i);
+            status=true;
+        }
+    }
+    if(!status){
+       
+
+    let errormsg=document.getElementById("search");
+    errormsg.textContent="No Content Found"
+   
+    }
+    
+}
+function getTaskListFromStorage() {
+    const savedTaskList = localStorage.getItem('taskInputList');
+    return savedTaskList ? JSON.parse(savedTaskList) : [];
+}
+function showTaskDetails(index) {
+    // Store the selected task in LocalStorage (optional)
+    localStorage.setItem('selectedTask', JSON.stringify(taskInputList[index]));
+
+    // Open the taskDetails.html file in a new tab or window
+    window.open('taskDetails.html', '_blank');
+}
+function saveTaskListToStorage() {
+    localStorage.setItem('taskInputList', JSON.stringify(taskInputList));
+}
+function handleSwipeLeft(taskItem, index) {
+    let touchStartX = 0;
+
+    taskItem.addEventListener('touchstart', function (event) {
+        touchStartX = event.touches[0].clientX;
+    });
+
+    taskItem.addEventListener('touchend', function (event) {
+        let touchEndX = event.changedTouches[0].clientX;
+        if (touchEndX < touchStartX) {
+            // Swipe left action: Implement what you want to do when a swipe left occurs
+            handleDelete(index);
+        }
+    });
+}
+function handleSwipeRight(taskItem, index) {
+    let touchStartX = 0;
+
+    taskItem.addEventListener('touchstart', function (event) {
+        touchStartX = event.touches[0].clientX;
+    });
+
+    taskItem.addEventListener('touchend', function (event) {
+        let touchEndX = event.changedTouches[0].clientX;
+        if (touchEndX > touchStartX) {
+            // Swipe right action: Implement what you want to do when a swipe right occurs
+            // For example, you might want to show the full details of the task on a new page.
+            showTaskDetails(index);
+        }
+    });
+}
+
+
+function handleDelete(index){
+    taskInputList.splice(index, 1);
+    saveTaskListToStorage();
+    showList();
+
+
+
+}
+function handleUpdate(index){
+    let newtaskinfo=prompt("Please Enter the msg",taskInputList[index]);
+    if(newtaskinfo){
+        taskInputList[index].taskInput=newtaskinfo;
+    }
+    saveTaskListToStorage();
+    showList();
+}
+function truncateText(text, maxLength) {
+    if (text.length > maxLength) {
+        return text.substring(0, maxLength) + '...';
+    }
+    return text;
+}
+function showList() {
+    let maxLines=1;
+    let maxCharsPerLine=15;
+    const ulList = document.getElementById("todoList");
+    ulList.innerHTML='';
+
+    for (let i = 0; i < taskInputList.length; i++) {
+        const newLi = document.createElement('li');
+        const heading=document.createElement('h3');
+        const Taskdiscription=document.createElement('p');
+        const newdel=document.createElement('button');
+        const newupdate=document.createElement('button')
+        newdel.textContent="Delete";
+        newupdate.textContent="Update";
+        heading.textContent=`${taskInputList[i].taskTitleInput}`;
+        Taskdiscription.textContent=truncateText(taskInputList[i].taskInput, maxCharsPerLine * maxLines)
+        ulList.appendChild(newLi);
+        newLi.appendChild(heading);
+        newLi.appendChild(Taskdiscription);
+        newLi.appendChild(newdel);
+        newLi.appendChild(newupdate);
+        heading.addEventListener('click', function() {
+            showTaskDetails(i);
+        });
+        newdel.addEventListener('click',()=>{
+            handleDelete(i);
+        })
+        newupdate.addEventListener('click',()=>{
+            handleUpdate(i);
+        })
+        handleSwipeLeft(newLi, i);
+        handleSwipeRight(newLi,i);
+
+    }
+}
